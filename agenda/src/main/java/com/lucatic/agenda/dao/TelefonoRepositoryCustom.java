16249bda04acc53b8ffa4d7236c6db74b07dac01
michael <michaelvargascastro@hotmail.com>
2019-05-16 15:11:54 +0200

    telefonoRepository

---
+++ agenda/src/main/java/com/lucatic/agenda/dao/TelefonoRepositoryCustom.java
@@ -0,0 +1,15 @@
+package com.lucatic.agenda.dao;
+
+import java.util.List;
+
+
+import com.lucatic.agenda.model.Telefono;
+
+public interface TelefonoRepositoryCustom {
+
+	
+	
+	//IMPLEMENTARLA TELEFONO Y COMPROBARLO
+	List<Telefono> getTelefono(String telefono);
+	
+}
