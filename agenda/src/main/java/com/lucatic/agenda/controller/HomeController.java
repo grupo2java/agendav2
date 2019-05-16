97d75a394ee1cb7b1827e3ffbb1b9a7d579cab1a
josepcorreira <josepcorreira@hotmail.com>
2019-05-16 21:37:28 +0200

    Update HomeController.java

--- agenda/src/main/java/com/lucatic/agenda/controller/HomeController.java
+++ agenda/src/main/java/com/lucatic/agenda/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.lucatic.agenda.controller;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -13,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.lucatic.agenda.model.Persona;
 import com.lucatic.agenda.services.PersonaService;
 
@@ -22,15 +23,15 @@ public class HomeController {
 
 	@Autowired
 	private PersonaService persoService;
-
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	@RequestMapping(value = "/" )
 	public String inicio(ModelMap model) {
-		System.out.println("ENTRAMOS EN EL INDEX DEL PROGRAMA");
+		logger.info("ENTRAMOS EN EL INDEX DEL PROGRAMA");
 		return "index"; 
 	}
 	@GetMapping("/listarContactos")
 	public String listaContactos(ModelMap model) {
-		System.out.println("entramos lista de contactos");
+		logger.info("entramos lista de contactos");
 		model.addAttribute("personas", persoService.list());
 
 		return "listaContactos";
@@ -40,25 +41,24 @@ public class HomeController {
 	//ESTO PREGUNTAR SI ES COORRECTO O MEJOR UTILIZAR POSTMAPPING 11MVPARAM PREGUNTAR GRUPO
 	@GetMapping("/volver")
 	public String volverInicio() {
-		System.out.println("volvemos al inicio");
+		logger.info("volvemos al inicio");
 		return "redirect:/";	
 	}
 	
 	@GetMapping("/new")
 	public String newUser(ModelMap model) {
-		
+		logger.info("creamos nueva persona");
 		model.addAttribute("persona", new Persona());
 		return "formContacto";		
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	
 	public ModelAndView saveUser(@RequestParam(value="nombre",required=false) String nombre ,
 			@RequestParam(value="apellido1",required=false) String apellido1,
 			@RequestParam(value="apellido2",required=false) String apellido2,
 			@RequestParam(value="dni",required=false) String dni,
 			@RequestParam(value ="nacimiento") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDateTime fecha) {
-	
+		
 		persoService.add(new Persona());
 		return new ModelAndView("redirect:/");
 	}
