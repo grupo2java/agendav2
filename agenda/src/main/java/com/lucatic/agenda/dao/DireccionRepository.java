afdc4542b31f3113c749ae084fc3f8aedfe232da
josepcorreira <josepcorreira@hotmail.com>
2019-05-20 17:06:17 +0200

    update 23

--- agenda/src/main/java/com/lucatic/agenda/dao/DireccionRepository.java
+++ agenda/src/main/java/com/lucatic/agenda/dao/DireccionRepository.java
@@ -4,6 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.lucatic.agenda.model.Direccion;
 
+/**
+ * 
+ * @author grupo2
+ * 
+ * @Version 2.0
+ * 
+ * fecha:15/05/2019
+ * 
+ * Descripcion:Repositorio para la clase direccion.
+ * 
+ * 
+ *
+ */
+
+
 @Repository
 public interface DireccionRepository extends JpaRepository<Direccion, Integer>{
 	
